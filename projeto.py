4fed730b96ee4dc472be1bc7d7ad490cecceb264
Matheus Parizotto <matheusparizotto29@gmail.com>
2024-12-31 10:45:29 -0400

    Removendo prints e tratando erros

--- projeto.py
+++ projeto.py
@@ -2,23 +2,26 @@
 import os
 from tkinter.filedialog import askdirectory
 import shutil
+import datetime
 
 pasta_selecionada = askdirectory()
-print(pasta_selecionada)
-
 lista_arquivos = os.listdir(pasta_selecionada)
-print(lista_arquivos)
 
 # Fazer o backup dos arquivos que estão nessa pasta
 nome_pasta_backup = "Backup"
 nome_completo_pasta_backup = f"{pasta_selecionada}/{nome_pasta_backup}"
 if not os.path.exists(nome_completo_pasta_backup):
     os.mkdir(nome_completo_pasta_backup)
+
+data_atual = datetime.date.today()
     
 for arquivo in lista_arquivos:
-    print(arquivo)
     nome_completo = f"{pasta_selecionada}/{arquivo}"
-    nome_final_arquivo = f"{nome_completo_pasta_backup}/{arquivo}"
+    nome_final_arquivo = f"{nome_completo_pasta_backup}/{data_atual}/{arquivo}"
+
+    if not os.path.exists(f"{nome_completo_pasta_backup}/{data_atual}"):
+        os.mkdir(f"{nome_completo_pasta_backup}/{data_atual}")
+
     if "." in arquivo:
         shutil.copy2(nome_completo, nome_final_arquivo)
     elif "Backup" != arquivo:
